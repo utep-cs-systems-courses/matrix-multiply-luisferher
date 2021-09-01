f6662327efd5b6b170f3c7e7b71fec256b6a9e65
luisferher <78455145+luisferher@users.noreply.github.com>
2021-08-31 23:53:10 -0600

    Update matrixUtils.py
    Created the multMatrix() function and ran several tests to determine the time to perform this function.

--- matrixUtils.py
+++ matrixUtils.py
@@ -1,8 +1,10 @@
+#Luis F. Hernandez
 #!/usr/bin/env python3
+import time
 import argparse
 import numpy as np
 
-def genMatrix(size=1024, value=1):
+def genMatrix(size, value):
     """
     Generates a 2d square matrix of the specified size with the specified values
     """
@@ -11,7 +13,7 @@ def genMatrix(size=1024, value=1):
 
     return matrix
 
-def genMatrix2(size=1024, value=1):
+def genMatrix2(size, value):
     """
     Generates a 2d square matrix of the specified size with the specified values
     """
@@ -19,6 +21,23 @@ def genMatrix2(size=1024, value=1):
     matrix = np.asarray([ np.asarray([value for col in range(0,size)]) for row in range(0,size)])
 
     return matrix
+
+def multMatrix(matA,matB):
+    
+    """
+    Multiply 2 2d square matrices and generates a 2d square matrix with the result of the multiplication operation
+    """
+    matrix = genMatrix2(256,0)
+    for x in range(len(matA)):
+        for y in range(len(matB[0])):
+            for k in range(len(matB)):
+                matrix[x][y] = ((matA[x][k]) * (matB[k][y]))
+
+    return matrix
+
+            
+
+
 
 def printSubarray(matrix, size=10):
     """
@@ -63,25 +82,32 @@ def main():
 
     parser = argparse.ArgumentParser(description=
         'Generate a 2d matrix and save it to  a file.')
-    parser.add_argument('-s', '--size', default=1024, type=int,
+    parser.add_argument('-s', '--size', default=256, type=int,
         help='Size of the 2d matrix to generate')
-    parser.add_argument('-v', '--value', default=1, type=int,
+    parser.add_argument('-v', '--value', default=2, type=int,
         help='The value with which to fill the array with')
     parser.add_argument('-f', '--filename',
         help='The name of the file to save the matrix in (optional)')
 
     args = parser.parse_args()
-
-    mat = genMatrix(args.size, args.value)
-
-    if args.filename is not None:
-        print(f'Writing matrix to {args.filename}')
-        writeToFile(mat, args.filename)
-
-        print(f'Testing file')
-        printSubarray(readFromFile(args.filename))
-    else:
-        printSubarray(mat)
+    #Get the first Matrix
+    mat1 = genMatrix2(args.size, args.value)
+    #Get the second Matrix 
+    mat2 = genMatrix2(args.size, args.value)
+    #Create the resulting Matrix
+    mat3 = genMatrix2(256,0)
+    totalTime = 0
+    for i in range(10):
+        print(f'Test '+str(i))
+        start = time.time() 
+        #Call the multiplication function and store the resulting matrix in mat3
+        mat3 = multMatrix(mat1,mat2)
+        elapsedTime = time.time() - start
+        totalTime += elapsedTime
+        printSubarray(mat3)
+        print(f'Elapsed Time: '+str(elapsedTime)+' Seconds')
+    totalTime = totalTime/10
+    print(f'Averaged Time per test: '+str(totalTime))
 
 if __name__ == '__main__':
     # execute only if run as a script
